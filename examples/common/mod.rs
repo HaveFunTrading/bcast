5bd73a7543b0cd2805d9b2442043c56d56356f07
Tom Brzozowski <tom.brzozowski@gsr.io>
2024-11-25 17:46:29 +0000

    ipc examples

--- examples/common/mod.rs
+++ examples/common/mod.rs
@@ -1,6 +1,7 @@
 use bcast::RingBuffer;
 use rand::{thread_rng, Rng};
 
+/// Generate random message every 1 millisecond.
 #[allow(dead_code)]
 pub fn writer(bytes: &[u8]) -> anyhow::Result<()> {
     let mut writer = RingBuffer::new(bytes).into_writer();
@@ -15,6 +16,9 @@ pub fn writer(bytes: &[u8]) -> anyhow::Result<()> {
     }
 }
 
+/// Consume messages produced by the writer. It will also perform payload validation (using user
+/// defined field set by the writer) and sleep for 10 milliseconds in order to process messages
+/// in a batch.
 #[allow(dead_code)]
 pub fn reader(bytes: &[u8]) -> anyhow::Result<()> {
     let mut reader = RingBuffer::new(bytes).into_reader();

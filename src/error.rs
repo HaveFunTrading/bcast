54cb9fb76edb214aec04fe5ad56696be7f5c6369
Tom Brzozowski <tom.brzozowski1985@gmail.com>
2024-12-05 15:51:20 +0000

    update docs

--- src/error.rs
+++ src/error.rs
@@ -7,10 +7,13 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Error types.
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum Error {
+    /// Consumer is unable to keep up with the producer.
     #[error("overran by the producer, reader position: {0}")]
     Overrun(usize),
+    /// The buffer used to read the message is too small.
     #[error("provided buffer is of insufficient size, provided: {0}, required: {1}")]
     InsufficientBufferSize(usize, usize),
+    /// Attempting to publish a message that exceeds MTU.
     #[error("mtu limit exceeded, requested: {0}, mtu: {1}")]
     MtuLimitExceeded(usize, usize),
 }
